91662e4943022ccbfb8de137947a6189fe0f88fa
Leonel Meque <leomeque@gmail.com>
2024-07-12 08:19:46 +0200

    first commit

---
+++ src/main/kotlin/com/learning/springboot/learnspringboot/LearnspringbootApplication.kt
@@ -0,0 +1,11 @@
+package com.learning.springboot.learnspringboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnspringbootApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearnspringbootApplication>(*args)
+}
